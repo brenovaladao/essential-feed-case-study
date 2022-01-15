223662ba1c46e3ea3f712133fd3b0e463a9bd631
Breno Valadão <brenovvaladao@gmail.com>
2022-01-15 15:45:22 +0100

    Extract duplicate test helpers into a shared scope

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -139,12 +139,4 @@ class CacheFeedUseCaseTests: XCTestCase {
         }
         return (models, local)
     }
-    
-    private func anyURL() -> URL {
-        return URL(string: "http://any-url.com")!
-    }
-    
-    private func anyNSError() -> NSError {
-        return NSError(domain: "any error", code: 1)
-    }
 }
