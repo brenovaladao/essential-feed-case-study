2b893431711d79425f8957a1f51908fad156575b
Breno Valadão <brenovvaladao@gmail.com>
2022-09-04 15:49:32 +0200

    Setup window and delete main storyboard

--- EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -11,7 +11,17 @@ import EssentialFeediOS
 
 class SceneDelegateTests: XCTestCase {
 
-    func test_sceneWillConnectToSession_configuresRootViewController() {
+//    func test_configureWindow_setsWindowAsKeyAndVisible() {
+//        let window = UIWindowSpy()
+//        let sut = SceneDelegate()
+//        sut.window = window
+//
+//        sut.configureWindow()
+//
+//        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1, "Expected to make window key and visible")
+//    }
+
+    func test_configureWindow_configuresRootViewController() {
         let sut = SceneDelegate()
         sut.window = UIWindow()
 
