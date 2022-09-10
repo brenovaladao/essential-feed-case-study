4d1e89a8d4470c750644666121b475290ebedcf1
Breno Valadão <brenovvaladao@gmail.com>
2022-09-10 10:51:30 +0200

    Composition with Combine

--- EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
+++ EssentialApp/EssentialApp/FeedLoaderPresentationAdapter.swift
@@ -5,28 +5,33 @@
 //  Created by Breno Valadão on 03/09/22.
 //
 
+import Combine
 import EssentialFeed
 import EssentialFeediOS
 
 final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-    private let feedLoader: FeedLoader
+    private let feedLoader: () -> FeedLoader.Publisher
+    private var cancellable: Cancellable?
+
     var presenter: FeedPresenter?
 
-    init(feedLoader: FeedLoader) {
+    init(feedLoader: @escaping () -> FeedLoader.Publisher) {
         self.feedLoader = feedLoader
     }
 
     func didRequestFeedRefresh() {
         presenter?.didStartLoadingFeed()
 
-        feedLoader.load { [weak self] result in
-            switch result {
-            case let .success(feed):
+        cancellable = feedLoader()
+            .dispatchOnMainQueue()
+            .sink(receiveCompletion: { [weak self] completion in
+                switch completion {
+                case let .failure(error):
+                    self?.presenter?.didFinishLoadingFeed(with: error)
+                case .finished: break
+                }
+            }, receiveValue: { [weak self] feed in
                 self?.presenter?.didFinishLoadingFeed(with: feed)
-
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingFeed(with: error)
-            }
-        }
+            })
     }
 }
